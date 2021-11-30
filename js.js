98f1e98b1a14e58bd09444f0dafed27879af89fb
BajGG <94964291+BajGG@users.noreply.github.com>
2021-11-30 10:51:18 +0000

    added event list

--- js.js
+++ js.js
@@ -1,7 +1,5 @@
 function login() {
-  document.getElementById("signup").style.backgroundColor = getComputedStyle(
-    document.querySelector(":root")
-  ).getPropertyValue("--nord3");
+  document.getElementById("signup").style.backgroundColor = "var(--nord3)";
   document.getElementById("login").style.backgroundColor = getComputedStyle(
     document.querySelector(":root")
   ).getPropertyValue("--nord1");
@@ -15,3 +13,19 @@ function signup() {
     document.querySelector(":root")
   ).getPropertyValue("--nord1");
 }
+
+document.getElementById("login").addEventListener("mousedown", () => {
+  document.getElementById("login").style.backgroundColor = "var(--nord2)";
+});
+
+document.getElementById("login").addEventListener("mouseup", () => {
+  document.getElementById("login").style.backgroundColor = "var(--nord1)";
+});
+
+document.getElementById("signup").addEventListener("mousedown", () => {
+  document.getElementById("signup").style.backgroundColor = "var(--nord2)";
+});
+
+document.getElementById("signup").addEventListener("mouseup", () => {
+  document.getElementById("signup").style.backgroundColor = "var(--nord1)";
+});
